a475961bdc989a6a5b6ab2653ea0d362f0185d6e
Donghee Han <hdh7485@gmail.com>
2019-10-29 10:00:49 +0900

    Add path maker and publisher

---
+++ dr_2dnav/scripts/odom_subscriber.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import rospy
+#from nav_msgs.msg import Odometry
+from geometry_msgs.msg import PoseWithCovarianceStamped
+
+def callback(data):
+    rospy.loginfo(data.pose)
+    f = open("path.txt",'a')
+    data = str(data.pose.pose.position.x) + ' ' + str(data.pose.pose.position.y) + ' ' + str(data.pose.pose.orientation.z) + ' ' + str(data.pose.pose.orientation.w) + ' ' + str(data.pose.pose.orientation.w) + '\n'
+    f.write(data)
+    f.close()
+    
+def listener():
+    rospy.init_node('listener', anonymous=True)
+    #rospy.Subscriber("/odom", Odometry, callback)
+    rospy.Subscriber("/amcl_pose", PoseWithCovarianceStamped, callback)
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()

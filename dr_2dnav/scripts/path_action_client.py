a32bf388a92c60d94990cbd5c625b6990b19ff5a
Donghee Han <hdh7485@gmail.com>
2019-10-29 16:51:45 +0900

    Add path_action_client.py

---
+++ dr_2dnav/scripts/path_action_client.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+import rospy
+from nav_msgs.msg import Path
+from std_msgs.msg import Header
+from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import Pose
+import tf
+
+from mbf_msgs.msg import ExePathAction
+
+def smach_client(path_msg):
+    client = actionlib.SimpleActionClient('mbf', ExePathAction)
+    client.wait_for_server()
+    goal = mbf_msgs.msg.ExePathGoal(path = path_msg)
+    client.send_goal = goal
+    client.wait_for_result()
+    return client.get_result()
+
+def talker():
+    f = open("path.txt", 'r')
+    lines = f.readlines()
+    f.close()
+    posestamp_list = []
+    seq = 0
+
+    header_msg = Header()
+    header_msg.seq = seq
+    header_msg.stamp = rospy.Time.now()
+    header_msg.frame_id = "map"
+
+    for line in lines:
+        path_msg = Path()
+        posestamp_msg = PoseStamped()
+        pose_msg = Pose()
+        value = line.split()
+        rospy.loginfo(value)
+        pose_msg.position.x = float(value[0])
+        pose_msg.position.y = float(value[1])
+        pose_msg.position.z = 0
+        pose_msg.orientation.x = 0
+        pose_msg.orientation.y = 0
+        pose_msg.orientation.z = float(value[2])
+        pose_msg.orientation.w = float(value[3])
+        posestamp_msg.pose = pose_msg
+        posestamp_msg.header = header_msg
+        posestamp_list.append(posestamp_msg)
+    rospy.loginfo(posestamp_list)
+
+    header_msg.seq = seq
+    header_msg.stamp = rospy.Time.now()
+    header_msg.frame_id = "map"
+
+    path_msg.header = header_msg
+    path_msg.poses = posestamp_list
+
+    return path_msg
+
+if __name__ == '__main__':
+    try:
+        rospy.init_node('path_publisher', anonymous=True)
+        path_msg = talker()
+        result = smach_client(path_msg)
+        rospy.loginfo(result)
+    except rospy.ROSInterruptException:
+        pass
